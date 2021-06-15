a84fd56beba696e508555dfaf6fa7f3cefdfdaa2
姬昌·君(Jichang Jun Djpeity) <36454945+JichangJunDjpeity@users.noreply.github.com>
2021-06-15 14:03:41 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,91 @@
-#include <stdio.h>
+#define 取 #include
+#define 吾 <
+#define 道法 stdio.h
+#define 阅之 >
+#define 或有一数乃 int //通用
+#define 或有一术乃 int //专用于函数
+#define 者也 ()
+#define 也 )
+#define 万物之始 main
+#define 其名曰 {
+#define 万物竞方者 switch(
+#define 此致 }
+#define 告之曰 return(
+#define 往复之若 while(
+#define 者为 =
+#define 者非 !=
+#define 如若 case
+#define 云云 break
+#define 如若不然 default
+/******************************************************************************/
+#define 执旗 Flag
+#define 嬉戏 game
+#define 曲有误 error
+/******************************************************************************/
+#define 初 0
+/******************************************************************************/
+#define 戏法之初
+#define 戏法竞牌
+#define 戏法始动
+#define 戏法轮转
+#define 戏法复回
+#define 戏法断陨
+#define 万元归于混沌 Init
+#define 标榜 Menu
+#define 风林火山 Setup_for_Run
+#define 开天辟地 Clear
+#define 执牛耳 Get_Input
+#define 思山河 Do_Logic
+#define 阅千卷 Render_Frame
+#define 侯佳音 Wait
+#define 涅槃重生 Fixup
+/******************************************************************************/
 
-int surf_internet() {
-  return 0;
-}
+取 吾道法阅之
 
-int main() {
-  return surf_internet();
-}
+或有一数乃 嬉戏执旗者为戏法之初;
+或有一数乃 曲有误者为初;
+
+或有一术乃万物之始者也
+其名曰
+    往复之若嬉戏执旗者非戏法断陨;
+    其名曰
+        万物竞方者嬉戏执旗也
+        其名曰
+            如若 戏法之始:  
+            其名曰
+                万元归于混沌者也;
+                嬉戏执旗者为戏法竞牌;
+            此致 云云;
+            如若 戏法竞牌:  
+            其名曰
+                嬉戏执旗者为标榜者也;
+            此致 云云;
+            如若 戏法始动:  
+            其名曰
+                风林火山者也;
+                嬉戏执旗者为戏法轮转;
+            此致 云云;
+            如若 戏法轮转:  
+            其名曰
+                开天辟地者也;
+                执牛耳者也;
+                思山河者也;
+                阅千卷者也;
+                候佳音者也;
+            此致 云云;
+            如若 戏法复回:  
+            其名曰
+                涅槃重生者也;
+                嬉戏执旗者为戏法竞牌;
+            此致 云云;
+            如若 戏法断陨:  
+            其名曰
+                曲有误者为初;
+            此致 云云;
+            如若不然: 云云;
+         此致
+    此致
+    告之曰曲有误也;
+此致
+
